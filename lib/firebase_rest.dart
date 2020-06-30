c5db4c47c8b25bb9a320dd78b13319253ff21f34
Anuar Otynshin <strigeandro@gmail.com>
2020-06-30 09:44:33 +0600

    rename the main class, implement singleton

--- lib/firebase_rest.dart
+++ lib/firebase_rest.dart
@@ -1,7 +1,7 @@
 library firebase_rest;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class FirebaseRest {
+  static final instance = FirebaseRest._();
+
+  FirebaseRest._();
 }

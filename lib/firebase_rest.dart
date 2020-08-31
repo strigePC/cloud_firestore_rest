1368a1d90e7693a18dfe4691eb22fcfae1c4f0ef
Anuar Otynshin <strigeandro@gmail.com>
2020-09-01 02:12:20 +0600

    add print

--- lib/firebase_rest.dart
+++ lib/firebase_rest.dart
@@ -1,7 +1,11 @@
 library firebase_rest;
 
+import 'package:firebase_core/firebase_core.dart';
+
 class FirebaseRest {
   static final instance = FirebaseRest._();
 
-  FirebaseRest._();
+  FirebaseRest._() {
+    print(Firebase.app().options.asMap);
+  }
 }

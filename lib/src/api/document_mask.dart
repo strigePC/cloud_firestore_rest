0634a701e87858e80069fc2dd058262393d429d4
Anuar Otynshin <strigeandro@gmail.com>
2020-09-07 02:23:49 +0600

    add documentation to DocumentMask class

--- lib/src/api/document_mask.dart
+++ lib/src/api/document_mask.dart
@@ -1,9 +1,21 @@
 part of cloud_firestore_rest;
 
+/// A set of field paths on a document. Used to restrict a get or update
+/// operation on a document to a subset of its fields. This is different from
+/// standard field masks, as this is always scoped to a [Document], and takes in
+/// account the dynamic nature of [Value].
+@JsonSerializable()
 class DocumentMask {
+  /// The list of field paths in the mask. See [Document.fields] for a field
+  /// path syntax reference.
   final List<String> fieldPaths;
 
   DocumentMask(this.fieldPaths) : assert(fieldPaths != null);
+
+  factory DocumentMask.fromJson(Map<String, dynamic> json) =>
+      _$DocumentMaskFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DocumentMaskToJson(this);
 
   @override
   String toString() {

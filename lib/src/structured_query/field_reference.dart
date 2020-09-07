0082d0314691a49f80fca4ea6a6598736f807e91
Anuar Otynshin <strigeandro@gmail.com>
2020-09-07 12:35:18 +0600

    add documentation to FieldReference

--- lib/src/structured_query/field_reference.dart
+++ lib/src/structured_query/field_reference.dart
@@ -1,10 +1,11 @@
 part of cloud_firestore_rest;
 
+/// A reference to a field, such as max(messages.time) as max_time.
 @JsonSerializable()
 class FieldReference {
   final String fieldPath;
 
-  FieldReference(this.fieldPath);
+  FieldReference(this.fieldPath) : assert(fieldPath != null);
 
   factory FieldReference.fromJson(Map<String, dynamic> json) =>
       _$FieldReferenceFromJson(json);
